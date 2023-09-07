0abda1bec1c3115cb5919c7e1e4e4899041dcefe
melamri-mea-fusion <143818814+melamri-mea-fusion@users.noreply.github.com>
2023-09-07 10:43:00 +0100

    Update migrateList.ps1

--- migrateList.ps1
+++ migrateList.ps1
@@ -2,12 +2,12 @@ Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
 Install-Module -Name "PnP.PowerShell" -RequiredVersion 1.12.0 -Force -AllowClobber
 Import-Module PnP.PowerShell
 #Connect to the Source Site
-Connect-PnPOnline -ClientId '9afea424-5d73-428b-b9ea-e0d87d400831' -CertificatePath ".\MEAFUSION.pfx" -CertificatePassword (ConvertTo-SecureString -AsPlainText $MEAFUSION_PFX_PASSWORD -Force) -Url https://meafusion.sharepoint.com/sites/DEV -Tenant "meafusion.onmicrosoft.com" 
+Connect-PnPOnline -ClientId '9afea424-5d73-428b-b9ea-e0d87d400831' -CertificatePath ".\MEAFUSION.pfx" -CertificatePassword (ConvertTo-SecureString -AsPlainText $Env:MEAFUSION_PFX_PASSWORD -Force) -Url https://meafusion.sharepoint.com/sites/DEV -Tenant "meafusion.onmicrosoft.com" 
 #Create the Template
 Get-PnPSiteTemplate -Out "TemplateFile.pnp" -ListsToExtract "Gift ideas" -Handlers Lists
   
 #Connect to Target Site
-Connect-PnPOnline -ClientId '9afea424-5d73-428b-b9ea-e0d87d400831' -CertificatePath ".\MEAFUSION.pfx" -CertificatePassword (ConvertTo-SecureString -AsPlainText $MEAFUSION_PFX_PASSWORD -Force) -Url https://meafusion.sharepoint.com/sites/UAT -Tenant "meafusion.onmicrosoft.com" 
+Connect-PnPOnline -ClientId '9afea424-5d73-428b-b9ea-e0d87d400831' -CertificatePath ".\MEAFUSION.pfx" -CertificatePassword (ConvertTo-SecureString -AsPlainText $Env:MEAFUSION_PFX_PASSWORD -Force) -Url https://meafusion.sharepoint.com/sites/UAT -Tenant "meafusion.onmicrosoft.com" 
  
 #Apply the Template
 Invoke-PnPSiteTemplate -Path "TemplateFile.pnp"
