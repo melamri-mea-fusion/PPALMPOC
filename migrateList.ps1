c6a139d0f1cbf4bdca992ba76a269acd5f2a56fe
melamri-mea-fusion <143818814+melamri-mea-fusion@users.noreply.github.com>
2023-09-07 09:44:41 +0100

    Update migrateList.ps1

--- migrateList.ps1
+++ migrateList.ps1
@@ -7,7 +7,7 @@ Connect-PnPOnline -ClientId '9afea424-5d73-428b-b9ea-e0d87d400831' -CertificateP
 Get-PnPSiteTemplate -Out "TemplateFile.pnp" -ListsToExtract "Gift ideas" -Handlers Lists
   
 #Connect to Target Site
-Connect-PnPOnline -ClientId '9afea424-5d73-428b-b9ea-e0d87d400831' -CertificatePath "C:\Users\PC\Desktop\SP PoC\MEAFUSION.pfx" -CertificatePassword (ConvertTo-SecureString -AsPlainText "smysmy9393@@" -Force) -Url https://meafusion.sharepoint.com/sites/UAT -Tenant "meafusion.onmicrosoft.com" 
+Connect-PnPOnline -ClientId '9afea424-5d73-428b-b9ea-e0d87d400831' -CertificatePath ".\MEAFUSION.pfx" -CertificatePassword (ConvertTo-SecureString -AsPlainText "smysmy9393@@" -Force) -Url https://meafusion.sharepoint.com/sites/UAT -Tenant "meafusion.onmicrosoft.com" 
  
 #Apply the Template
 Invoke-PnPSiteTemplate -Path "TemplateFile.pnp"
