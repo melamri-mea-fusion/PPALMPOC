a2f2622ec53f6aa23601f68bbd1245aa15e619e5
melamri-mea-fusion <143818814+melamri-mea-fusion@users.noreply.github.com>
2023-09-07 09:41:02 +0100

    Update migrateList.ps1

--- migrateList.ps1
+++ migrateList.ps1
@@ -1,5 +1,5 @@
 Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
-Install-Module -Name "PnP.PowerShell"
+Install-Module -Name "PnP.PowerShell" -RequiredVersion 1.12.0 -Force -AllowClobber
 Import-Module PnP.PowerShell
 #Connect to the Source Site
 Connect-PnPOnline -ClientId '9afea424-5d73-428b-b9ea-e0d87d400831' -CertificatePath ".\MEAFUSION.pfx" -CertificatePassword (ConvertTo-SecureString -AsPlainText "smysmy9393@@" -Force) -Url https://meafusion.sharepoint.com/sites/DEV -Tenant "meafusion.onmicrosoft.com" 
