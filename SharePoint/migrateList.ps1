bf5bc5ae9bc003b9e369ca6e2a8e2208a2ed92f1
melamri-wdigis <mehdi.elamri@mea-fusion.ma>
2023-09-08 10:41:59 +0100

    Update migrateList.ps1

--- SharePoint/migrateList.ps1
+++ SharePoint/migrateList.ps1
@@ -11,7 +11,7 @@ Write-Output "PFX file generated ... OK"
 Connect-PnPOnline -ClientId '9afea424-5d73-428b-b9ea-e0d87d400831' -CertificatePath $pfxFileName -CertificatePassword (ConvertTo-SecureString -AsPlainText $Env:MEAFUSION_PFX_PASSWORD -Force) -Url https://meafusion.sharepoint.com/sites/DEV -Tenant "meafusion.onmicrosoft.com" 
 Write-Output "Connect to the Source Site ... OK"
 
-$jsonData = Get-Content -Path "./lists.json" -Raw | ConvertFrom-Json
+$jsonData = Get-Content -Path ".\SharePoint\lists.json" -Raw | ConvertFrom-Json
 foreach ($current_property in $jsonData.PSObject.Properties) {
     Write-Host "$($current_property.Name): $($current_property.Value)"
 }
