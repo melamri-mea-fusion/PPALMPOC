3f422b855f1bb0e6654695c32ebff47c86661e31
melamri-wdigis <mehdi.elamri@mea-fusion.ma>
2023-09-08 10:56:53 +0100

    Update migrateList.ps1

--- SharePoint/migrateList.ps1
+++ SharePoint/migrateList.ps1
@@ -13,13 +13,12 @@ Write-Output "Connect to the Source Site ... OK"
 
 $lists = Get-Content -Path ".\SharePoint\lists.json" -Raw | ConvertFrom-Json
 foreach ($list in $lists) {
-    Write-Host "List Name: $list"
+    Get-PnPSiteTemplate -Out "TemplateFile.xml" -ListsToExtract $list -Handlers Lists
+    Write-Output "Create the Template for $list ... OK"
+
+    Connect-PnPOnline -ClientId '9afea424-5d73-428b-b9ea-e0d87d400831' -CertificatePath $pfxFileName -CertificatePassword (ConvertTo-SecureString -AsPlainText $Env:MEAFUSION_PFX_PASSWORD -Force) -Url https://meafusion.sharepoint.com/sites/UAT -Tenant "meafusion.onmicrosoft.com" 
+    Write-Output "Connect to Target Site ... OK"
+
+    Invoke-PnPSiteTemplate -Path "TemplateFile.xml"
+    Write-Output "$list migration to Target Site ... OK"
 }
-Get-PnPSiteTemplate -Out "TemplateFile.xml" -ListsToExtract "Gift ideas" -Handlers Lists
-Write-Output "Create the Template ... OK"
-
-Connect-PnPOnline -ClientId '9afea424-5d73-428b-b9ea-e0d87d400831' -CertificatePath $pfxFileName -CertificatePassword (ConvertTo-SecureString -AsPlainText $Env:MEAFUSION_PFX_PASSWORD -Force) -Url https://meafusion.sharepoint.com/sites/UAT -Tenant "meafusion.onmicrosoft.com" 
-Write-Output "Connect to Target Site ... OK"
-
-Invoke-PnPSiteTemplate -Path "TemplateFile.xml"
-Write-Output "Apply the Template ... OK"
