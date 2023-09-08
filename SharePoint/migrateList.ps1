e0e79f51af1fbdcfe98960306955c87c262869f2
melamri-wdigis <mehdi.elamri@mea-fusion.ma>
2023-09-08 10:49:01 +0100

    Update migrateList.ps1

--- SharePoint/migrateList.ps1
+++ SharePoint/migrateList.ps1
@@ -11,9 +11,9 @@ Write-Output "PFX file generated ... OK"
 Connect-PnPOnline -ClientId '9afea424-5d73-428b-b9ea-e0d87d400831' -CertificatePath $pfxFileName -CertificatePassword (ConvertTo-SecureString -AsPlainText $Env:MEAFUSION_PFX_PASSWORD -Force) -Url https://meafusion.sharepoint.com/sites/DEV -Tenant "meafusion.onmicrosoft.com" 
 Write-Output "Connect to the Source Site ... OK"
 
-$jsonData = Get-Content -Path ".\SharePoint\lists.json" -Raw | ConvertFrom-Json
-foreach ($current_property in $jsonData.PSObject.Properties) {
-    Write-Host "$($current_property.Name): $($current_property.Value)"
+$lists = Get-Content -Path ".\SharePoint\lists.json" -Raw | ConvertFrom-Json
+foreach ($list in $lists) {
+    Write-Host "List Name: $(list)"
 }
 Get-PnPSiteTemplate -Out "TemplateFile.xml" -ListsToExtract "Gift ideas" -Handlers Lists
 Write-Output "Create the Template ... OK"
